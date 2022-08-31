4d47f7e9228adcb591f708396733086a44aa5cd4
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-08-31 12:30:20 -0400

    Update buf_ovrflow.cpp

--- buf_ovrflow.cpp
+++ buf_ovrflow.cpp
@@ -1,29 +1,7 @@
-#include <cstdio>
-#include <cstring>
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-const char *PASSWORD_FILE = "rictro";
-
-int main()
-{
-  char input[8];
-  char password[8];
-
-  sscanf(PASSWORD_FILE, "%s", password);
-
-  cout << "Enter password: ";
-  cin >> input;
-
-  // Debug prints:
-  // std::cout << "Address of input: " << &input << "\n";
-  // std::cout << "Address of password: " << &password << "\n";
-  // std::cout << "Input: " << input << "\n";
-  // std::cout << "Password: " << password << "\n";
-
-  if (std::strncmp(password, input, 8) == 0)
-    cout << "Access granted\n";
-  else
-    cout << "Access denied\n";
-
-  return 0;
+int main(void){
+ int buffer[10];
+  buffer[11] = 7;
 }
