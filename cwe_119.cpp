caa37963ec87a7ee67209067a6891f10fa8ebd46
Jeffrey Matthew <32321280+Jeffrey-Matthew@users.noreply.github.com>
2022-09-05 17:27:28 -0400

    Create cwe_119.cpp

---
+++ cwe_119.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+/*The following example asks a user for an offset into an array to select an item.*/
+int GetUntrustedOffset()
+{
+    int x = -1;
+    return x;
+}
+
+int main(int argc, char **argv)
+{
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = GetUntrustedOffset();
+
+    std::cout << "You selected \n"
+              << items[index - 1];
+}
